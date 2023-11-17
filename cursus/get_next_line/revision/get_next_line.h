23bdc288e40cfe2159abae2440e4de4f3511ed25
Daniella Andrea Mazzini Henriquez <damazzin@c3r3s1.42malaga.com>
2023-11-17 16:59:11 +0100

    Se agrega actualizado para revision

---
+++ cursus/get_next_line/revision/get_next_line.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: damazzin <damazzin@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/09 12:25:41 by damazzin          #+#    #+#             */
+/*   Updated: 2023/11/17 14:23:35 by damazzin         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1
+# endif
+
+# include <fcntl.h>
+# include <libc.h>
+# include <stdio.h>
+# include <string.h>
+# include <unistd.h>
+
+size_t	ft_strlen(const char *str);
+char	*ft_strjoin(char *s1, const char *s2);
+int		ft_strchr(const char *s, int c);
+char	*ft_strdup(const char *src);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dst_size);
+char	*get_next_line(int fd);
+
+#endif
