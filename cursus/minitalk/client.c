7270aae4d6ecb28873d857a49e3a8853e61044d5
Daninja111 <mazzinidaniella@gmail.com>
2024-03-11 14:27:06 +0100

    Se agrega avance server y client

--- cursus/minitalk/client.c
+++ cursus/minitalk/client.c
@@ -10,3 +10,45 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <signal.h>
+#include <unistd.h>
+
+void ft_bin(char c, int pid) {
+    int count = 0;
+
+    while (count < 8) { // Process each of the 8 bits of the character
+        // Determine the bit value at the current position by shifting
+        // Shift 'c' right by (7 - cnt) to bring the desired bit to the LSB position
+        // AND with 1 to isolate the LSB
+        int bit = (c >> (7 - count)) & 1;
+
+        if (bit) { // bit = 1
+            kill(pid, SIGUSR1);
+        } else { // bit = 0
+            kill(pid, SIGUSR2);
+        }
+
+        count++;
+        usleep(100);
+    }
+}
+//atoi libft
+int main(int argc, char *argv[]){
+  char  *str;
+  int pid;
+  int count;
+
+  if(argc == 3){
+    count = 0;
+    str = argv[2];
+    pid = ft_atoi(argv[1]); // [0] file [1] PID [2] String
+    if(!pid)
+      return(1); // pid doesn't exist: some error
+    while(str[count]){
+      ft_bin(str[count], pid);//ft para enviar cada caracter y pid
+      count++;
+    }
+    //ft_bin(flag, pid)enviar un digito para marcar el final
+  }
+  return(0);
+}
--- cursus/minitalk/server.c
+++ cursus/minitalk/server.c
@@ -10,3 +10,25 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+#include <signal.h>
+#include <stdio.h>
+
+//ft bin to char
+//ft_putnbr_fd del libft
+int main(void){
+	struct sigaction	sa;
+
+	sa.sa_flags = SA_SIGINFO;
+	sa.sa_sigaction = ft_bin_to_char;
+	write(1, "PID: ", 5);
+	ft_putnbr_fd(getpid(), 1);
+	write(1, "\n", 1);
+	while (1)
+	{
+		sigaction(SIGUSR1, &sa, NULL);
+		sigaction(SIGUSR2, &sa, NULL);
+		pause();
+	}
+	return (0);
+}
